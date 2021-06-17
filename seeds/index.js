1b831ba8c8262e732a5dcbfc204ca36bd44bfb92
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-06-17 17:20:58 -0500

    finished seed data and logic

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers.js');
 const Fishinghole = require('../models/fishingHole');
 
 mongoose.connect('mongodb://localhost:27017/fishing-hole', {
@@ -14,11 +15,30 @@ db.once('open', () => {
     console.log('Database connected!');
 });
 
-//clear everything in DB 
+//pass in an array and return random elements from that array 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
 const seedDB = async () => {
+    //clear everything in DB 
     await Fishinghole.deleteMany({});
-    const c = new Fishinghole({title: 'Bellas Fishing Hole'});
-    await c.save();
+    //loop through 50xs to get a random city/state
+    for(let i = 0; i < 50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const hole = new Fishinghole({
+            //set new fishing hole to random city/state
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+             //give a random decriptor and place for title 
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        //save 
+        await hole.save(); 
+    }
 }
 
 seedDB(); 
+
+//close database connection 
+seedDB().then(() => {
+    mongoose.connection.close(); 
+})
--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -29,7 +29,7 @@ module.exports.places = [
     'Ghost Town',
     'Cut',
     'Corner',
-    'Backcountry',
+    'Swamp',
     'River',
     'Creek',
     'Creekside',
