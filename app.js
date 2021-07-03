23982f0ad74248507b82b7e5c57015b4e8db59af
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-07-03 13:21:53 -0500

    moved review routes to a sep file/folder

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path'); 
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { fishingholeSchema, reviewSchema } = require('./schemas.js');
-const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
-const Fishinghole = require('./models/fishingHole');
-const Review = require('./models/review');
+
+
 const fishingholes = require('./routes/fishingholes');
+const reviews = require('./routes/reviews');
+
 
 
 mongoose.connect('mongodb://localhost:27017/fishing-hole', {
@@ -35,64 +35,19 @@ mongoose.set('useFindAndModify', false);
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-//function to validate fishinghole w/ JOI validation middleware
-const validateFishinghole = (req, res, next) => {
-const { error } = fishingholeSchema.validate(req.body);
-if(error) {
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400);
-} else {
-    next();
-    }
-}
 
 
 //app.use path and router name
 app.use('/fishingholes', fishingholes);
-
-
-//function to validate reviews w/ JOI validation middleware
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body); 
-    if(error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    } else {
-        next();
-    }
-}
-
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use('/fishingholes/:id/reviews', reviews);
 
 
 
+//home
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
 
-//POST (submits the data from the review form)
-app.post('/fishingholes/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    //use the id to find the fishinghole 
-    const fishinghole = await Fishinghole.findById(req.params.id);
-    //make a new review
-    const review = new Review(req.body.review);
-    //push onto fishinghole.reviews the new review
-    fishinghole.reviews.push(review);
-    //save
-    await review.save();
-    await fishinghole.save(); 
-    //redirect back to the fishinghole show page
-    res.redirect(`/fishingholes/${fishinghole._id}`);
-}));
-
-
-//DELETE route to delete a review for a fishinghole 
-app.delete('/fishingholes/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Fishinghole.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/fishingholes/${id}`);
-}));
 
 
 app.all('*', (req, res, next) => {
---
+++ routes/reviews.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const catchAsync = require('../utilities/catchAsync');
+const ExpressError = require('../utilities/ExpressError');
+const { reviewSchema } = require('../schemas.js');
+
+const Fishinghole = require('../models/fishingHole');
+const Review = require('../models/review');
+
+
+//function to validate reviews w/ JOI validation middleware
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body); 
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+
+
+//POST (submits the data from the review form)
+router.post('/', validateReview, catchAsync(async (req, res) => {
+    //use the id to find the fishinghole 
+    const fishinghole = await Fishinghole.findById(req.params.id);
+    //make a new review
+    const review = new Review(req.body.review);
+    //push onto fishinghole.reviews the new review
+    fishinghole.reviews.push(review);
+    //save
+    await review.save();
+    await fishinghole.save(); 
+    //redirect back to the fishinghole show page
+    res.redirect(`/fishingholes/${fishinghole._id}`);
+}));
+
+
+//DELETE route to delete a review for a fishinghole 
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Fishinghole.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/fishingholes/${id}`);
+}));
+
+
+module.exports = router;
