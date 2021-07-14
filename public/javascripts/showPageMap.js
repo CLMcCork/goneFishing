f25641137a614346d2f13680398e7deab12bbbc0
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-07-14 18:06:02 -0500

    added a custom map popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -10,6 +10,12 @@ const map = new mapboxgl.Map({
 
 new mapboxgl.Marker()
     .setLngLat(fishinghole.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+        .setHTML (
+            `<h5>${fishinghole.title}</h5><p>${fishinghole.location}</p>`
+        )
+    )
     .addTo(map);
 
    
