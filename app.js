d7150e3f2158edadde69ba14a5646289aab74dc0
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-07-20 14:57:00 -0500

    sanitized HTML w/ JOI

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const passport = require('passport');
 const localStrategy = require('passport-local'); 
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 const userRoutes = require('./routes/users');
 const fishingholeRoutes = require('./routes/fishingholes');
@@ -45,6 +47,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'thiswillbeasecrethereeventually',
--- schemas.js
+++ schemas.js
@@ -1,14 +1,34 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
 
-//this is NOT mongoose schema--this is just a pattern for a javascript 
-//object that has has been defined in our schema.js file using JOI methods
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
+
 module.exports.fishingholeSchema = Joi.object({
     fishinghole: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
-        //image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
@@ -16,6 +36,6 @@ module.exports.fishingholeSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        body: Joi.string().required().escapeHTML()
     }).required()
-});
+})
