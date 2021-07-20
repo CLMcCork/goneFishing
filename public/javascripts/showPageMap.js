c4db505c2016f44a6e3262e6cc976c578a00b638
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-07-19 21:36:08 -0500

    map styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -8,6 +8,8 @@ const map = new mapboxgl.Map({
     zoom: 10 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
     .setLngLat(fishinghole.geometry.coordinates)
     .setPopup(
